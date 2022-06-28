57fa68a5dbb8c11a2314c0e8edd50f84f21379f4
Brett Tackaberry <brett@rebel.com>
2022-06-27 22:58:24 -0400

    New presenetation

---
+++ src/components/DemoThree/WorkingWIthMe.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+
+
+const WorkingWIthMe = () => {
+    return (
+        <div id="career-overview" className="about-area border-bottom-two three ptb-100">
+            <div className="container align-items-center">
+                <div className="row align-items-center">
+
+                    <div>
+                        <div className="about-content about-content--techforgood">
+                            <div id="ttt" className="section-title three">
+
+                                <span className="sub-title">Working With Me</span>
+
+                                
+                            </div>
+                        </div>
+                    </div>
+                   
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default WorkingWIthMe
