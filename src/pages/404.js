387cf511d92b108c0abe49f38ea32467bd51aefd
Brett Tackaberry <brett@rebel.com>
2023-06-12 11:11:49 -0400

    Fix date

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,11 @@
 import React from "react"
 
-import Layout from "../components/Layout/layout"
-import Seo from "../components/Layout/seo"
+import Layout from "../components/Layout/Layout"
+import Seo from "../components/Layout/Seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <Seo title="404: Not found" />
+    <Seo pageTitle="404: Not found" />
     <h1>404: Not Found</h1>
     <p>It'll be ok, we can find it together.</p>
   </Layout>
